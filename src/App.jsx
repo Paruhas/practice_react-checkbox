c1342ae295665a82dfa7716ee31fe07cb3160750
Paruhas <bayulgang@hotmail.com>
2021-08-03 15:20:56 +0700

    logic checkall css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import mockData from "./data.json";
 
 function App() {
   const [allData, setAllData] = useState([]);
+  const [isCheckAll, setIsCheckAll] = useState({
+    check: false,
+    type: "",
+  });
 
   const getTableData = async () => {
     const res = await mockData;
@@ -24,6 +28,7 @@ function App() {
         return { ...item, isChecked: checked };
       });
       setAllData(newDataWhenIsCheckedChange);
+      setIsCheckAll((prev) => ({ ...prev, check: checked, type: "all" }));
     }
 
     if (name !== "check-all") {
@@ -35,6 +40,24 @@ function App() {
         }
       });
       setAllData(newDataWhenIsCheckedChange);
+
+      newDataWhenIsCheckedChange.map((item) => {
+        if (item.isChecked !== false) {
+          console.log("not all");
+          setIsCheckAll((prev) => ({
+            ...prev,
+            check: checked,
+            type: "single",
+          }));
+          return item;
+        }
+        if (item.isChecked === true) {
+          console.log("yes it's all");
+          setIsCheckAll((prev) => ({ ...prev, type: "all" }));
+          return item;
+        }
+        return item;
+      });
     }
   };
 
@@ -50,10 +73,11 @@ function App() {
                     <input
                       type="checkbox"
                       name="check-all"
-                      className="default-input select-all"
+                      className="default-input"
+                      checked={isCheckAll.check}
                       onChange={(e) => handleChangChecked(e)}
                     />
-                    <span className="custom-checkbox"></span>
+                    <span className="custom-checkbox-all"></span>
                     <span className="on-hover-checkbox"></span>
                   </label>
                 </th>
@@ -98,15 +122,18 @@ function App() {
           .app-content {
             padding-bottom: 3rem;
           }
+
           .app-table {
             width: 50%;
             border-collapse: collapse;
             margin: auto;
           }
+
           .app-table > tbody > tr {
             border-bottom: 1px solid black;
             height: 5rem;
           }
+
           .app-table > tbody > tr > th:nth-child(1) {
             width: 10%;
             text-align: center;
@@ -117,6 +144,7 @@ function App() {
           .app-table > tbody > tr > th:nth-child(3) {
             width: 80%;
           }
+
           .app-table > tbody > tr > td:nth-child(1) {
             width: 10%;
             text-align: center;
@@ -129,6 +157,7 @@ function App() {
           }
         
           /* ----- CheckBox ----- */
+
           .checkbox-container {
             position: relative;
             cursor: pointer;
@@ -141,6 +170,16 @@ function App() {
             width: 0;
             cursor: pointer;
           }
+
+          .custom-checkbox-all {
+            position: absolute;
+            margin: -0.75rem 0px 0px -0.5rem;
+            height: 1.5rem;
+            width: 1.5rem;
+            background-color: #ffffff;
+            border: 2px solid #c7c7c7;
+            border-radius: 5px;
+          }
           .custom-checkbox {
             position: absolute;
             margin: -0.75rem 0px 0px -0.5rem;
@@ -150,7 +189,9 @@ function App() {
             border: 2px solid #c7c7c7;
             border-radius: 5px;
           }
+
           /* ----- When Hover ----- */
+
           .on-hover-checkbox {
             position: absolute;
             margin: -1.5rem 0px 0px -1.25rem;
@@ -161,28 +202,49 @@ function App() {
             opacity: 0;
             transition: opacity 0.5s;
           }
+
           .checkbox-container:hover .default-input ~ .on-hover-checkbox {
             opacity: 1;
           }
           .checkbox-container:hover .default-input ~ .custom-checkbox {
             border-color: #000000;
           }
+
           /* ----- When Checked(BG and CheckMark indicator Style) ----- */
+
+          .checkbox-container .default-input:checked ~ .custom-checkbox-all {
+            background-color: #000000;
+            border-color: #000000;
+          }
           .checkbox-container .default-input:checked ~ .custom-checkbox {
             background-color: #000000;
             border-color: #000000;
           }
+
           /* hidden when not checked */
+
+          .custom-checkbox-all:after {
+            content: "";
+            position: absolute;
+            display: none;
+          }
           .custom-checkbox:after {
             content: "";
             position: absolute;
             display: none;
           }
+
           /* Show the custom-checkbox when checked */
+
+          .checkbox-container .default-input:checked ~ .custom-checkbox-all:after {
+            display: block;
+          }
           .checkbox-container .default-input:checked ~ .custom-checkbox:after {
             display: block;
           }
+
           /* Style the custom-checkbox/indicator */
+
           .checkbox-container .custom-checkbox:after {
             left: 0.3rem;
             top: 0;
@@ -198,6 +260,41 @@ function App() {
               45deg
             );
           }
+
+          ${
+            isCheckAll.type === "all"
+              ? `.checkbox-container .custom-checkbox-all:after {
+              left: 0.3rem;
+              top: 0;
+              width: 35%;
+              height: 70%;
+              border: solid white;
+              border-width: 0 3px 3px 0;
+              -webkit-transform: rotate(
+                45deg
+              );
+              -ms-transform: rotate(45deg);
+              transform: rotate(
+                45deg
+              );
+            }`
+              : `.checkbox-container .custom-checkbox-all:after {
+              left: 0.51rem;
+              top: 0.25rem;
+              width: 0;
+              height: 50%;
+              border: solid white;
+              border-width: 0 3px 3px 0;
+              -webkit-transform: rotate( 
+                90deg
+              ); 
+              -ms-transform: rotate(90deg);
+              transform: rotate( 
+                90deg
+              );
+            }`
+          }
+
         `}
       </style>
     </>
